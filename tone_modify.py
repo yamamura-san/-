fa6efb2771914405468e23c5d6f4249aa8ad618d
yamamura-san <125817325+yamamura-san@users.noreply.github.com>
2023-02-28 23:43:02 +0900

    Add files via upload

---
+++ tone_modify.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import math
+
+#transrate input image into the other image with different tone
+class ToneColor:
+    def tone(self, image, ganma):
+        img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+        self.ganma = math.floor(ganma * 10) / 10
+        
+        # tone curve function
+        x =  np.arange(256)
+        y =  (x/255)**ganma * 255
+        dst = cv2.LUT(img, y)
+        cv2.imwrite("ave_tone_{0}.jpg".format(self.ganma), dst)
+
+        dst = dst.astype(np.uint8)
+        dst_color = cv2.applyColorMap(dst, cv2.COLORMAP_JET)
+        cv2.imwrite("ave_color_tone_{0}.jpg".format(self.ganma), dst_color)
+
+if __name__ == "__main__":
+    img_tone = ToneColor()
+    img_tone.tone("ave.jpg", 1/10)
