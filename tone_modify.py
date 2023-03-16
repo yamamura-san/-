60709406239f353f0cfdd76c21ac4638edd3e041
yamamura-san <125817325+yamamura-san@users.noreply.github.com>
2023-03-17 00:36:24 +0900

    Update tone_modify.py

--- tone_modify.py
+++ tone_modify.py
@@ -6,18 +6,20 @@ import math
 class ToneColor:
     def tone(self, image, ganma):
         img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+        self.img = image
         self.ganma = math.floor(ganma * 10) / 10
         
         # tone curve function
         x =  np.arange(256)
         y =  (x/255)**ganma * 255
         dst = cv2.LUT(img, y)
-        cv2.imwrite("ave_tone_{0}.jpg".format(self.ganma), dst)
+        cv2.imwrite(self.img +"_ganma_{0}.jpg".format(self.ganma), dst)
 
         dst = dst.astype(np.uint8)
         dst_color = cv2.applyColorMap(dst, cv2.COLORMAP_JET)
-        cv2.imwrite("ave_color_tone_{0}.jpg".format(self.ganma), dst_color)
+        cv2.imwrite("ave_color_ganma_{0}.jpg".format(self.ganma), dst_color)
 
 if __name__ == "__main__":
     img_tone = ToneColor()
-    img_tone.tone("ave.jpg", 1/10)
+    img_tone.tone("ave.jpg", 2)
+
